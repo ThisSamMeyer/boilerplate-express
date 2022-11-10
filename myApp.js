aa85f7c91079fcf3691a0052f95ea43a663ca4a6
Samantha Meyer <slmeyer90@gmail.com>
2022-11-10 14:58:53 -0500

    added env variable

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
 
 app.use("/public", express.static(__dirname + "/public"))
 
@@ -9,9 +10,16 @@ app.get('/', (req,res) => {
 })
 
 app.get('/json', (req,res) => {
-    res.json({
-        "message": "Hello json"
-    })
+    if (process.env.MESSAGE_STYLE == "uppercase"){
+        res.json({
+            "message": "HELLO JSON"
+        })
+    }else {
+        res.json({
+            "message": "Hello json"
+        })
+    }
+
 })
 
 console.log("Hello World")
