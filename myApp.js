22c12902018443b62959ffc3473d6add22f64b18
Samantha Meyer <slmeyer90@gmail.com>
2022-11-10 14:16:32 -0500

    serve views/index.html

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
 
+let absolutePath = __dirname + "/views/index.html"
+
 app.get('/', (req,res) => {
-    res.send('Hello Express')
+    // res.send('Hello Express')
+    res.sendFile(absolutePath)
 })
 
 console.log("Hello World")
