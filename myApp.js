75010c5987d55399d837323db200bb7b17fdff8b
Samantha Meyer <slmeyer90@gmail.com>
2022-11-10 14:46:07 -0500

    added /json path

--- myApp.js
+++ myApp.js
@@ -8,6 +8,12 @@ app.get('/', (req,res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
+app.get('/json', (req,res) => {
+    res.json({
+        "message": "Hello json"
+    })
+})
+
 console.log("Hello World")
 
 
