3c3aec85d54e428ceeb3d94b502f70efacf5e323
Samantha Meyer <slmeyer90@gmail.com>
2022-11-10 21:16:04 -0500

    added query param

--- myApp.js
+++ myApp.js
@@ -1,31 +1,32 @@
+// Basic Node and Express
+
 let express = require('express')
 let app = express()
+
+// #6a - Use the .env File
 require('dotenv').config()
 
+// #4 - Serve Static Assets
 app.use("/public", express.static(__dirname + "/public"))
 
+// #7 - Implement a Root-Level Request Logger Middleware
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`)
     next();
 })
 
 app.get('/', (req, res) => {
+    // #2 - Start a Working Express Server
     // res.send('Hello Express')
+
+    // #3 - Serve an HTML File
     res.sendFile(__dirname + "/views/index.html")
 })
 
-app.get('/now', (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-}, (req, res) => {
-    res.json( { "time": req.time } );
-});
-
-app.get('/:word/echo', (req, res) => {
-    res.json( {"echo": req.params.word});
-});
-
 app.get('/json', (req, res) => {
+    // res.json( { "message": "Hello json" } )  // #5 - Serve JSON on a Specific Route
+
+    // #6b - Use the .env File
     if (process.env.MESSAGE_STYLE === "uppercase"){
         res.json({
             "message": "Hello json".toUpperCase()
@@ -37,6 +38,24 @@ app.get('/json', (req, res) => {
     }
 })
 
-console.log("Hello World");
+// #8 - Chain Middleware to Create a Time Server
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json( { "time": req.time } );
+});
+
+// #9 - Get Route Parameter Input from the Client
+app.get('/:word/echo', (req, res) => {
+    res.json( { "echo": req.params.word } );
+});
+
+// #10 - Get Query Parameter Input from the Client
+app.get(`/name?first=firstname&last=lastname`, (req, res) => {
+    res.json( { "name": `${firstname} ${lastname}` } )
+})
+
+console.log("Hello World"); // #1 - Meet the Node Console
 
  module.exports = app;
